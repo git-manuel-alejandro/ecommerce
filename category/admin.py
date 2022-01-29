89ceb9a4d87aeb0e148da2a41df808f4250ca90b
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-29 15:46:23 -0300

    setting slug

--- category/admin.py
+++ category/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .import models
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name' , )}
+    list_display = ('name' , 'slug')
 
-admin.site.register(models.Category)
+admin.site.register(models.Category , CategoryAdmin)
